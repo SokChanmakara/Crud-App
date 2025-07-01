a0bf8658f060b7284434c760806267cee726ff20
SokChanmakara <sokchanmakara04@gmail.com>
2025-07-02 03:03:19 +0700

    add express route and errorHandler middleware

---
+++ backend/src/middleware/errorHandler.js
@@ -0,0 +1,34 @@
+const errorHandler = (err, req, res, next) => {
+  console.error("Error:", err);
+
+  //Default Error
+  let error = {
+    success: false,
+    message: err.message || "Internal Server Error",
+    ...err(process.env.NODE_ENV === "development" && { stack: err.stack }),
+  };
+  //SQL Server specific Error
+  if (err.code) {
+    switch (err.code) {
+      case "ELOGIN":
+        error.message = "Database authentication failed";
+        res.status(500);
+        break;
+      case "ETIMEOUT":
+        error.message = "Database connection timeout";
+        res.status(504);
+        break;
+      case "EREQUEST":
+        error.message = "Database request failed";
+        res.status(400);
+        break;
+      default:
+        res.status(500);
+    }
+  } else {
+    res.status(err.statusCode || 500);
+  }
+  res.json(error);
+};
+
+module.exports = errorHandler;
